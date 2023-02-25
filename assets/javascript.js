bf145b8717312f6751098f0cefb2cbef10cb9e9f
Kruton Cluster <125205934+kruton-cluster@users.noreply.github.com>
2023-02-25 17:03:08 +0530

    Add files via upload

---
+++ assets/javascript.js
@@ -0,0 +1,40 @@
+//window.addEventListener("beforeunload", function(event) {
+  //event.preventDefault();
+  //event.returnValue = ""; // required for Chrome
+  //event.message = "Out so soon? Sure you don't want high quality photos?";
+ //return event.message;
+//});
+
+function disableRightClick() 
+{ 
+alert("Sorry, right click is not allowed !!"); 
+return false; 
+} 
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
